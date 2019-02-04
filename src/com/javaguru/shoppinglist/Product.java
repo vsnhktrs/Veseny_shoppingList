2aa82b63fec0dc074e2faa59356398a51efb7e83
arturs.vesna"gmail.com <Esdarewq123>
2019-02-05 01:07:59 +0200

    feature from old repository

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -8,6 +8,9 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private String description;
+    private double discount;
 
     public Long getId() {
         return id;
@@ -33,28 +36,61 @@ public class Product {
         this.price = price;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Product product = (Product) o;
         return Objects.equals(id, product.id) &&
-                Objects.equals(name, product.name) &&
-                Objects.equals(price, product.price);
+            Objects.equals(name, product.name) &&
+            Objects.equals(price, product.price) &&
+            Objects.equals(category, product.category) &&
+            Objects.equals(discount, product.discount) &&
+            Objects.equals(description, product.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, price);
+        return Objects.hash(id, name, price, category, discount, description);
     }
 
     @Override
     public String toString() {
         return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", category='" + category + '\'' +
+            ", discount=" + discount + "%" +
+            ", description='" + description + '\'' +
+            '}';
     }
 }
