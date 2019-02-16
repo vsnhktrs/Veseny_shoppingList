be6338bb86262df8835e1f48e7c9fae80f3c005b
arturs.vesna"gmail.com <Esdarewq123>
2019-02-16 21:05:13 +0200

    try catch now works

--- src/com/javaguru/shoppinglist/service/validation/ProductNameValidationRule.java
+++ src/com/javaguru/shoppinglist/service/validation/ProductNameValidationRule.java
@@ -6,10 +6,10 @@ public class ProductNameValidationRule implements ProductValidationRule {
 
     @Override
     public void validate(Product product) {
-        int minNameLenght = 3;
-        int maxNameLenght = 32;
+        int minNameLetters = 3;
+        int maxNameLetters = 32;
 
-        if (product.getName().length() < minNameLenght & product.getName().length() > maxNameLenght) {
+        if (product.getName().length() < minNameLetters | product.getName().length() > maxNameLetters) {
             throw new ProductValidationException(
                 "Product name must have more then 3 and not more then 32 symbols");
         }
