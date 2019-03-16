a49b27f4a55af2032c001cd296717d0d88508bd0
arturs.vesna"gmail.com <Esdarewq123>
2019-03-16 16:51:59 +0200

    something made

--- src/main/java/com/javaguru/shoppinglist/repository/productIdSequence.java
+++ src/main/java/com/javaguru/shoppinglist/repository/productIdSequence.java
@@ -3,22 +3,38 @@ package com.javaguru.shoppinglist.repository;
 import com.javaguru.shoppinglist.domain.Product;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
 @Component
-public class productIdSequence {
+public class productIdSequence  {
 
     private Long TASK_ID_SEQUENCE = 0L;
     private Map<Long, Product> products = new HashMap<>();
 
-    public Product add(Product product) {
-        product.setId(TASK_ID_SEQUENCE);
-        products.put(TASK_ID_SEQUENCE, product);
-        TASK_ID_SEQUENCE++;
-        return product;
+    public Long add(Product product) {
+//        product.setId(TASK_ID_SEQUENCE);
+//        products.put(TASK_ID_SEQUENCE, product);
+//        TASK_ID_SEQUENCE++;
+//        return product;
+    product.setId(TASK_ID_SEQUENCE++);
+    products.put(product.getId(), product);
+    return product.getId();
     }
 
     public Product findId(Long id) {
         return products.get(id);
     }
+
+//    public Long add(Product product){
+//        product.setId(TASK_ID_SEQUENCE++);
+//        products.put(product.getId(), product);
+//        return product.getId();
+//    }
+//
+//    public Optional<Product> findId(Long id){
+//        return Optional.ofNullable(products.get(id));
+//    }
+
 }
